74b44931e504bbee0ce3d4e01d2503b946795d0f
pxlc <mike.aquino@industrialbrothers.com>
2019-01-23 15:10:23 -0500

    refined the TextOverlay class to provide ability to get pixel dimensions of a text string -- this changes behaviour of TextOverlay in that it now will only support one font size for any text added to it. Usage will require creating an new TextOverlay instance for other font size.

--- pxlc/img/_pil_utils.py
+++ pxlc/img/_pil_utils.py
@@ -8,7 +8,7 @@ PXLC_TD_ROOT = '/'.join(os.path.abspath(__file__).replace('\\','/').split('/')[:
 
 class TextOverlay(object):
 
-    def __init__(self, fill_color_str, stroke_color_str='', stroke_pixel_width=1, ttf_filepath=''):
+    def __init__(self, font_size, fill_color_str, stroke_color_str='', stroke_pixel_width=1, ttf_filepath=''):
 
         self.fill_color_str = fill_color_str
         self.stroke_color_str = stroke_color_str
@@ -21,21 +21,25 @@ class TextOverlay(object):
         else:
             self.ttf_filepath = '%s/res/font/DejaVuSansMono/DejaVuSansMono.ttf' % PXLC_TD_ROOT
 
-    def add_text_item(self, text_message, x, y, font_size):
+        self.font_size = font_size
+        self.font = ImageFont.truetype(self.ttf_filepath, size=font_size)
 
-        self.text_item_list.append({'text': text_message, 'pos': (x, y), 'font_size': font_size})
+    def add_text_item(self, text_message, x, y):
 
-    def _draw_text_message(self, draw_obj, x, y, text, font_size):
+        self.text_item_list.append({'text': text_message, 'pos': (x, y)})
 
-        font = ImageFont.truetype(self.ttf_filepath, size=font_size)
+    def get_text_size(self, text_message):
+        return self.font.getsize(text_message)   # return the size of the text in tuple: (width, height)
+
+    def _draw_text_message(self, draw_obj, x, y, text):
 
         if self.stroke_color_str is not None:
             stroke_thickness = self.stroke_pixel_width
             for st_y in range(0-stroke_thickness, stroke_thickness+1):
                 for st_x in range(0-stroke_thickness, stroke_thickness+1):
-                    draw_obj.text((x+st_x, y+st_y), text, font=font, fill=self.stroke_color_str)
+                    draw_obj.text((x+st_x, y+st_y), text, font=self.font, fill=self.stroke_color_str)
 
-        draw_obj.text((x, y), text, fill=self.fill_color_str, font=font)
+        draw_obj.text((x, y), text, fill=self.fill_color_str, font=self.font)
 
     def draw_items(self, pil_img_to_draw_on):
 
@@ -43,8 +47,7 @@ class TextOverlay(object):
         for t_item in self.text_item_list:
             text = t_item.get('text')
             (x, y) = t_item.get('pos')
-            f_size = t_item.get('font_size')
-            self._draw_text_message(draw_obj, x, y, text, f_size)
+            self._draw_text_message(draw_obj, x, y, text)
 
 
 def add_color_bkg_to_image( pil_img_w_alpha, bkg_rgba_int_tuple ):
